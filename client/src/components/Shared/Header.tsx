9330665d99912fdaa08e08fedb24dad9d1303cc2
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-09-20 10:48:36 +0700

    feat(components): add header component

---
+++ client/src/components/Shared/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="text-center mb-8">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Xếp Lịch Ca Thi Tự Động (Tô Màu Đồ Thị)
+      </h1>
+      <p className="text-lg text-gray-600 max-w-3xl mx-auto">
+        Sử dụng thuật toán tô màu đồ thị để sắp xếp các ca thi một cách tự động
+        nhưng vẫn đảm bảo các ràng buộc về thời gian và số ca thi.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
