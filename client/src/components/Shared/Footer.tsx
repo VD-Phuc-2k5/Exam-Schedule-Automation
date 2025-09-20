d0e5162dbf13dff6510088ead4423576869b809f
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-09-20 10:51:11 +0700

    feat(components): add footer component

---
+++ client/src/components/Shared/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="mt-12 text-center text-gray-600">
+      <p className="text-sm">
+        Ứng dụng minh họa thuật toán tô màu đồ thị - Phát triển bằng React
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
