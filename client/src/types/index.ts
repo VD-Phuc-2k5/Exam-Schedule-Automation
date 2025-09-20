0539d013e98e322bbec4714650a41f7102e8b8c1
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-09-20 13:12:44 +0700

    feat: add Subject and Student interfaces

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,1 +1,13 @@
 export type TabType = "theory" | "data" | "visualization" | "results";
+
+export interface Subject {
+  id: string;
+  name: string;
+  students: string[];
+}
+
+export interface Student {
+  id: string;
+  name: string;
+  subjects: string[];
+}
