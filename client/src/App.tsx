911a64aba6614c7a28b6c29d6fba672a0e74bb94
VD-Phuc-2k5 <duyvo1954@gmail.com>
2025-09-20 13:29:42 +0700

    feat: add data input tab

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
-import TheorySection from "./components/TheorySection";
-import Header from "./components/Shared/Header";
-import Footer from "./components/Shared/Footer";
-import Navigation from "./components/Shared/Navigation";
-import { type TabType } from "./types/";
+import TheorySection from "@/components/TheorySection";
+import Header from "@/components/Shared/Header";
+import Footer from "@/components/Shared/Footer";
+import Navigation from "@/components/Shared/Navigation";
+import DataInput from "@/components/DataInput";
+import { type TabType, type Subject, type Student } from "@/types";
+import { DEFAULT_SUBJECTS, DEFAULT_STUDENTS } from "@/data";
 
 function App() {
   const [activeTab, setActiveTab] = useState<TabType>("theory");
+  const [subjects, setSubjects] = useState<Subject[]>(DEFAULT_SUBJECTS);
+  const [students, setStudents] = useState<Student[]>(DEFAULT_STUDENTS);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
@@ -18,6 +22,16 @@ function App() {
         <div className="space-y-8">
           {activeTab === "theory" && <TheorySection />}
         </div>
+
+        {/* Du lieu */}
+        {activeTab === "data" && (
+          <DataInput
+            subjects={subjects}
+            students={students}
+            onSubjectsChange={setSubjects}
+            onStudentsChange={setStudents}
+          />
+        )}
 
         <Footer />
       </div>
